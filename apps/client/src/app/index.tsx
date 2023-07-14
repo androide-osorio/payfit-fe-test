8f87f15df8537bcd9637a367c753e80fc840a7e7
Andres Osorio <androideosorio@me.com>
2023-07-14 11:13:19 +0200

    add react query

--- apps/client/src/app/index.tsx
+++ apps/client/src/app/index.tsx
@@ -1,11 +1,21 @@
 import { RouterProvider } from 'react-router-dom'
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from '@tanstack/react-query';
+
 import { router } from './shared/router'
 import { ThemeProvider } from './components';
+
+const queryClient = new QueryClient();
 
 export function App() {
   return (
     <ThemeProvider>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
