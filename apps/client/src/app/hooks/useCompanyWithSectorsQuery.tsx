9f6505276c9e968062cdc38363dc86b4994e73b5
Andres Osorio <androideosorio@me.com>
2023-07-14 12:46:58 +0200

    add hook docs

--- apps/client/src/app/hooks/useCompanyWithSectorsQuery.tsx
+++ apps/client/src/app/hooks/useCompanyWithSectorsQuery.tsx
@@ -1,42 +1,64 @@
 import { useQuery } from '@tanstack/react-query';
 import { type components } from '@template/sdk';
+
 import { getCompanies, getSectors } from '../shared/http';
-import { type CompanyWithSectors, type Sector, type CompanyWithSectorsKeys } from '../types';
+import {
+  type CompanyWithSectors,
+  type Sector,
+  type CompanyWithSectorsKeys,
+} from '../types';
 
 type Params = components['schemas']['Schema']['findCompaniesQuerySchema'];
 
+/**
+ * Extracts an array of sector IDs from an array of companies with sectors.
+ * @param companies An array of companies with sectors.
+ * @returns An array of sector IDs.
+ */
 const extractSectorIds = (companies: CompanyWithSectorsKeys[]) => {
-	if (!companies) return undefined;
-	return companies.flatMap((company) =>
-		company.sectors.map((sector) => sector.sectorId)
-	);
-}
-
-const injectSectors = (companies: CompanyWithSectorsKeys[], sectors: Sector[]): CompanyWithSectors[] => {
-	return companies.map((company) => {
-		const companySectors = company.sectors
-			.map((sector) =>
-				sectors.find((s) => s.id === sector.sectorId)
-			)
-			.filter(Boolean);
-
-		return {
-			...company,
-			sectors: companySectors,
-		} as CompanyWithSectors;
-	});
+  if (!companies) return undefined;
+  return companies.flatMap((company) =>
+    company.sectors.map((sector) => sector.sectorId),
+  );
 };
 
+/**
+ * Injects sector information into an array of companies with sectors.
+ * @param companies An array of companies with sectors.
+ * @param sectors An array of sectors.
+ * @returns An array of companies with sectors and their corresponding sector information.
+ */
+const injectSectors = (
+  companies: CompanyWithSectorsKeys[],
+  sectors: Sector[],
+): CompanyWithSectors[] => {
+  return companies.map((company) => {
+    const companySectors = company.sectors
+      .map((sector) => sectors.find((s) => s.id === sector.sectorId))
+      .filter(Boolean);
+
+    return {
+      ...company,
+      sectors: companySectors,
+    } as CompanyWithSectors;
+  });
+};
+
+/**
+ * A custom hook that fetches companies with their corresponding sectors.
+ * @param options An object containing query parameters.
+ * @returns An object containing the loading state, error, and data of the companies with their corresponding sectors.
+ */
 export const useCompanyWithSectorsQuery = (options: Params = {}) => {
-	const {
+  const {
     isLoading: companiesLoading,
     error: companiesError,
     data: companies = [],
   } = useQuery({
     queryKey: ['companies'],
     queryFn: async () => {
-      const { total, results } = await getCompanies();
-      return results as CompanyWithSectorsKeys[];
+      const { results } = await getCompanies(options);
+      return results satisfies CompanyWithSectorsKeys[];
     },
   });
 
@@ -57,9 +79,9 @@ export const useCompanyWithSectorsQuery = (options: Params = {}) => {
 
   const companiesWithSectors = injectSectors(companies, sectors);
 
-	return {
-		isLoading: companiesLoading || sectorsLoading,
-		error: companiesError || sectorsError,
-		data: companiesWithSectors,
-	};
-}
+  return {
+    isLoading: companiesLoading || sectorsLoading,
+    error: companiesError || sectorsError,
+    data: companiesWithSectors,
+  };
+};
